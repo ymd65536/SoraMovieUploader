bca85b9df71964c5dc03faf2c6c3665b941ba810
ymd65536 <ymd65536@gmail.com>
2025-10-18 06:39:56 +0900

    Add launch configuration for .NET Core and update video generation prompt

--- Models/VideoGenerationRequest.cs
+++ Models/VideoGenerationRequest.cs
@@ -5,7 +5,7 @@ namespace DotNETSora.Models
     public class VideoGenerationRequest
     {
         [JsonPropertyName("prompt")]
-        public string Prompt { get; set; } = "A cat playing piano in a jazz bar.";
+        public string Prompt { get; set; } = "A young woman walks through a rainy street in Tokyo. Shallow depth of field. Neon lights reflect off the wet pavement. Ambient sound: rain and city noise.She stops under a red awning, takes out her phone, and says, dialogue: “I hope he’s still awake.” Close-up. Warm side light from a vending machine.";
 
         [JsonPropertyName("width")]
         public int Width { get; set; } = 480;
--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Core;
 
